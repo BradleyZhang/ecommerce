624a87d806132fc08fe96b87172f09b5e9dc3e6a
BradleyZhang <2111976953@qq.com>
2026-09-12 17:06:06 +0800

    cmd:monolith

---
+++ cmd/monolith/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/BradleyZhang/ecommerce/user/usecase"
+
+	"github.com/BradleyZhang/ecommerce/user/repository"
+
+	"github.com/BradleyZhang/ecommerce/user/plugins/validater"
+
+	"github.com/BradleyZhang/ecommerce/user/delivery/rest"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.New()
+	repo := repository.NewMemRepo()
+	validater := validater.NewTodoValidater()
+	usecase := usecase.NewUserUsecase(repo, validater)
+	handler := rest.NewUserHandler(usecase)
+	handler.SetRouter(server)
+
+	srv := &http.Server{
+		Addr:    ":1717",
+		Handler: server,
+	}
+
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		fmt.Print("failed to start HTTP server: " + err.Error())
+	}
+	fmt.Print("server exited")
+}
